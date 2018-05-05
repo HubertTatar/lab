c055a6ac181e6894ac91a8c4162b8c9c3e5a897c
Hubert Tatar <hutatar@gmail.com>
2018-05-05 18:02:40 +0200

    credit

---
+++ credit/src/main/java/io/huta/credit/Config.java
@@ -0,0 +1,9 @@
+package io.huta.credit;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+
+}
---
+++ credit/src/main/java/io/huta/credit/CreditApplication.java
@@ -0,0 +1,14 @@
+package io.huta.credit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@SpringBootApplication
+@EnableWebFlux
+public class CreditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditApplication.class, args);
+    }
+}
